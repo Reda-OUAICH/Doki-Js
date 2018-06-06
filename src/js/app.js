8bb951c7ce5207d58ccd40b43066f9af909f65a4
Réda Ouaich <33479820+Reda-OUAICH@users.noreply.github.com>
2018-06-06 18:46:50 +0200

    bug fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,24 @@
 // Replace ./data.json with your JSON feed
 fetch('./js/data.json').then(response => {  
   return response.json();
-}).then(data => {   // Work with JSON data here
-  image.src = data.steps[0].img_src;
-  questions.innerHTML = data.steps[0].description;
-  firstAnswer.innerHTML = data.steps[0].option1.description;
-  secondAnswer.innerHTML = data.steps[0].option2.description;
-}).catch(err => {   // Do something for an error here
-});
+}).then(data => {  
 
+  firstAnswer.addEventListener('click', function() {
+    index = data.steps[index].option1.next;
+    questions.innerHTML = data.steps[index].description;
+    firstAnswer.innerHTML = data.steps[index].option1.description;
+  });
+
+  secondAnswer.addEventListener('click', function() {
+    index = data.steps[index].option2.next;
+    questions.innerHTML = data.steps[index].description;
+    secondAnswer.innerHTML = data.steps[index].option2.description;
+  });
+
+  image.src = data.steps[0].img_src;
+}).catch(err => {  
+  console.log('ERROR DATA');
+});
 
 var questions = document.querySelector('#questions');
 var firstAnswer = document.querySelector('#answer1');
@@ -16,13 +26,18 @@ var secondAnswer = document.querySelector('#answer2');
 var image = document.querySelector('#character');
 var index = 0;
 
-function gameEngine(param1, param2, param3, param4) {
+
+function game(param1, param2, param3, param4) {
   questions.innerHTML = param1;
   firstAnswer.innerHTML = param2;
   secondAnswer.innerHTML = param3;
   image.src = param4;
 };
 
+// secondAnswer.addEventListener('click', function() {
+//   console.log('cucu');
+// });
+
 // function startGame(data) {
-// gameEngine(data.steps[0].description, data.steps[0].img_src, data.steps[0].option1.description);
+//   game(data.steps[0].description, data.steps[0].img_src, data.steps[0].option1.description);
 // }
